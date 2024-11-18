4fc91ec43fa785df002239930068ff18b51a374f
JackeyGame <3646561508@qq.com>
2024-11-18 14:49:54 +0800

    aaa

--- src/settings.6c452.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default","PlayerController","UIToast"],collisionMatrix:[[true,true],[true,true],[false,false,false]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scenes/Loading.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"52d36",resources:"404b4",main:"e829c"}};
